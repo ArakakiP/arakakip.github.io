80b6314ba981f972259d1a94710e4d9ffc695819
Guilherme Arakaki <guipecoisarakaki@gmail.com>
2023-02-03 12:27:00 -0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(animalInput ),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+      console.log(data)
+      setResult(data.result);
+      setAnimalInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h3>Me faça uma pergunta</h3>
+        <form onSubmit={onSubmit}>
+          <textarea
+            type="text"
+            name="animal"
+            placeholder="Pergunta"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Gerar Resposta" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
